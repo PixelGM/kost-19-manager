b998598833c43cfe0619189d47a9525c1030e9d7
ピクセルGM <amosprayogo@gmail.com>
2024-03-01 20:07:06 -0800

    App -> NewRoom rename

--- kost-19-manager/src/NewRoom.tsx
+++ kost-19-manager/src/NewRoom.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './NewRoom.css'
 
-function App() {
+function NewRoom() {
   const [room, setRoom] = useState({
     nomorKamar: '',
     hargaPerBulan: '',
@@ -104,4 +104,4 @@ function App() {
   );
 }
 
-export default App
+export default NewRoom;
--- kost-19-manager/src/main.tsx
+++ kost-19-manager/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import NewRoom from './NewRoom.tsx'
 import ListRooms from './ListRooms.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ListRooms />
+    <NewRoom />
   </React.StrictMode>,
 )
