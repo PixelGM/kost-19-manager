c2b1910c55417c6a3e539c8b4436c76523ff9244
ピクセルGM <amosprayogo@gmail.com>
2024-03-03 14:06:46 -0800

    Routing on index (/)

--- kost-19-manager/src/App.tsx
+++ kost-19-manager/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NewRoom from './NewRoom'; // Adjust the import path as necessary
+import ListRooms from './ListRooms';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/create/" element={<NewRoom />} />
+        <Route path="/" element={<ListRooms />} />
         {/* Define other routes here */}
       </Routes>
     </BrowserRouter>
